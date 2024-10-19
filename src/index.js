5f152f36a93f23be1222f6a5fe0cfbb604ea1d1c
SashikantaTripathy <162958055+SashikantaTripathy@users.noreply.github.com>
2024-10-19 15:44:53 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,65 +1,24 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
-const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema'); // Ensure this file exports typeDefs
 const resolvers = require('./resolvers'); // Ensure this file exports resolvers
 const jwt = require('jsonwebtoken');
-const dbHost = process.env.DB_HOST;
-// first require the package at the top of the file
 const helmet = require('helmet')
 const cors = require('cors');
-// add the middleware after app.use(helmet());
 const depthLimit = require('graphql-depth-limit');
 const { createComplexityLimitRule } = require('graphql-validation-complexity');
-
-
-
-
-if (!dbHost) {
-  console.error('DB_HOST environment variable is not set.');
-  process.exit(1);
-}
-
-db.connect(dbHost);
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 4000;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
-  context: async ({ req }) => {
-  // get the user token from the headers
-  const token = req.headers.authorization;
-  // try to retrieve a user with the token
-  const user = await getUser(token);
- // add the db models and the user to the context
- return { models, user };
- }
-});
 const app = express();
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.qbjjt.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.MONGO_DB}`).then(() => {app.listen(port);}).catch(err => (console.log(mongoose.Error)))
 app.use(helmet());
 app.use(cors());
 
-server.applyMiddleware({ app, path: '/api' });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
 
-app.listen({ port }, () =>
-  console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`)
-);
-const getUser = token => {
-  if(token){
-    try{
-      return jwt.verify(token,process.env.JWT_SECRET);
-    }
-    catch(err){
-      throw new Error('Session invalid');
-    }
-  }
-}
+
+
